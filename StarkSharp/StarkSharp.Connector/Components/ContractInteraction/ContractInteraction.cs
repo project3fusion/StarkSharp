eb73a55e38354d1e40f603282271c239164b153f
LastCeri <125711498+LastCeri@users.noreply.github.com>
2023-10-04 00:48:31 +0300

    Update ContractInteraction.cs

--- StarkSharp/StarkSharp.Connector/Components/ContractInteraction/ContractInteraction.cs
+++ StarkSharp/StarkSharp.Connector/Components/ContractInteraction/ContractInteraction.cs
@@ -1,4 +1,5 @@
-﻿using StarkSharp.Rpc.Utils;
+﻿using Newtonsoft.Json;
+using StarkSharp.Rpc.Utils;
 
 
 namespace StarkSharp.Connectors.Components
@@ -9,12 +10,18 @@ namespace StarkSharp.Connectors.Components
         public string EntryPoint { get; set; } 
         public string CallData { get; set; }    
 
-        public ContractInteraction(string _ContractAdress, string _EntryPoint , string _CallData) {
+        public ContractInteraction(string _ContractAdress, string _EntryPoint , object _CallData) 
+        {
+            ContractAdress = _ContractAdress;
+            EntryPoint = StarknetOps.CalculateFunctionSelector(_EntryPoint);
+            CallData = JsonConvert.SerializeObject(_CallData);
+        }
 
+        public ContractInteraction(string _ContractAdress, string _EntryPoint, string _CallData)
+        {
             ContractAdress = _ContractAdress;
             EntryPoint = StarknetOps.CalculateFunctionSelector(_EntryPoint);
             CallData = _CallData;
-
         }
     }
 }
