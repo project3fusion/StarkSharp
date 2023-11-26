d668952cf0383513ee9bba1dd83dde9a6341073e
TUsluRa <125711498+LastCeri@users.noreply.github.com>
2023-11-26 06:03:39 +0300

    Update & Fix StarkSharp.Base/Net <#049>

--- StarkSharp/StarkSharp.Base/StarkSharp.Net/NetClient.cs
+++ StarkSharp/StarkSharp.Base/StarkSharp.Net/NetClient.cs
@@ -1,36 +1,50 @@
-﻿using System;
+﻿using StarkSharp.Base.Cairo;
+using StarkSharp.Base.Net.Hash;
+using StarkSharp.Base.Net.Transaction;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
-
+using static StarkSharp.Base.Net.Models.NetModel;
+using static StarkSharp.Base.Net.Transaction.BaseTransactionComponent;
 
 namespace StarkSharp.Base.Net
 {
-    public class NetClient
+    public abstract class NetClient
     {
-        public abstract NetNetworks net { get; }
+        public abstract NetNetworks net { get;  }
 
-        public abstract StarknetBlock GetBlock(Hash? blockHash = null, int? blockNumber = null);
+        public abstract StarknetBlock GetBlock(NetHash? blockHash = null, int? blockNumber = null);
 
-        public abstract BlockTransactionTraces TraceBlockTransactions(Hash? blockHash = null, int? blockNumber = null);
+        public abstract BlockTransactionTraces TraceBlockTransactions(NetHash? blockHash = null, int? blockNumber = null);
 
-        public abstract BlockStateUpdate GetStateUpdate(Hash? blockHash = null, int? blockNumber = null);
+        public abstract BlockStateUpdate GetStateUpdate(NetHash? blockHash = null, int? blockNumber = null);
 
-        public abstract int GetStorageAt(Hash contractAddress, int key, Hash? blockHash = null, int? blockNumber = null);
+        public abstract int GetStorageAt(NetHash contractAddress, int key, NetHash? blockHash = null, int? blockNumber = null);
 
-        public abstract Transaction GetTransaction(Hash txHash);
+        public abstract BaseTransaction GetTransaction(NetHash txHash);
 
-        public abstract TransactionReceipt GetTransactionReceipt(Hash txHash);
+        public async Task<TransactionReceipt> GetTransactionReceipt(NetHash txHash)
+        {
+            var receipt = new TransactionReceipt
+            {
+                Id = 1,
+                Jsonrpc = "2.0",
+                Method = "starknet_getTransactionReceipt",
+                Params = new List<string> { txHash.ToString() }
+            };
 
-        public async Task<TransactionReceipt> WaitForTx(Hash txHash, bool? waitForAccept = null, float checkInterval = 2, int retries = 500)
+            return receipt;
+        }
+
+        public async Task<TransactionReceipt> WaitForTx(NetHash txHash, bool? waitForAccept = null, float checkInterval = 2, int retries = 500)
         {
             if (checkInterval <= 0)
-                throw new ArgumentException("Argument check_interval has to be greater than 0.");
+                throw new ArgumentException("Argument checkInterval has to be greater than 0.");
             if (retries <= 0)
                 throw new ArgumentException("Argument retries has to be greater than 0.");
             if (waitForAccept is not null)
-                warnings.warn("Parameter `wait_for_accept` has been deprecated - since Starknet 0.12.0, transactions in a PENDING"
-    
-                             " block have status ACCEPTED_ON_L2.");
+                Console.WriteLine("Parameter `waitForAccept` has been deprecated - since Starknet 0.12.0, transactions in a PENDING block have status ACCEPTED_ON_L2.");
 
             while (true)
             {
@@ -38,9 +52,11 @@ namespace StarkSharp.Base.Net
                 {
                     TransactionReceipt txReceipt = await this.GetTransactionReceipt(txHash);
 
-                    var deprecatedStatus = _statusToFinalityExecution(txReceipt.status);
-                    var finalityStatus = txReceipt.finalityStatus ?? deprecatedStatus[0];
-                    var executionStatus = txReceipt.executionStatus ?? deprecatedStatus[1];
+                    var deprecatedStatus = BaseTransactionComponent.StatusToFinalityExecution(txReceipt.status);
+                    var statusType = ConvertStringToStatusType(txReceipt.status.ToString());
+
+                    var executionStatus = statusType.ExecutionStatus;
+                    var finalityStatus = statusType.FinalityStatus;
 
                     if (executionStatus == TransactionExecutionStatus.REJECTED)
                         throw new TransactionRejectedError(message: txReceipt.rejectionReason);
@@ -51,59 +67,64 @@ namespace StarkSharp.Base.Net
                     if (executionStatus == TransactionExecutionStatus.SUCCEEDED)
                         return txReceipt;
 
-                    if (finalityStatus in (TransactionFinalityStatus.ACCEPTED_ON_L2, TransactionFinalityStatus.ACCEPTED_ON_L1))
-                    return txReceipt;
+                    if (new[] { TransactionFinalityStatus.ACCEPTED_ON_L2, TransactionFinalityStatus.ACCEPTED_ON_L1 }.Contains(finalityStatus))
+                        return txReceipt;
+
 
                     retries -= 1;
                     if (retries == 0)
                         throw new TransactionNotReceivedError();
 
-                    await Task.Delay(checkInterval);
+                    await Task.Delay((int)(checkInterval * 1000));
                 }
-                catch (TaskCanceledException ex)
+                catch (TaskCanceledException)
                 {
                     throw new TransactionNotReceivedError();
                 }
                 catch (ClientError ex)
                 {
-                    if ("Transaction hash not found" != ex.message)
-                        throw ex;
+                    if ("Transaction hash not found" != ex.Message)
+                        throw;
                     retries -= 1;
                     if (retries == 0)
                         throw new TransactionNotReceivedError();
 
-                    await Task.Delay(checkInterval);
+                    await Task.Delay((int)(checkInterval * 1000));
                 }
             }
         }
+        private StatusType ConvertStringToStatusType(string status)
+        {
+            if (Enum.TryParse<StatusType>(status, out var statusType))
+            {
+                return statusType;
+            }
 
-        public abstract EstimatedFee EstimateFee(AccountTransaction tx, Hash? blockHash = null, int? blockNumber = null);
+            throw new ArgumentException("Invalid status value.");
+        }
 
-        public abstract List<int> CallContract(Call call, Hash? blockHash = null, int? blockNumber = null);
+        public abstract EstimatedFee EstimateFee(AccountTransaction tx, NetHash? blockHash = null, int? blockNumber = null);
 
-        public async Task<SentTransactionResponse> SendTransaction(Invoke transaction)
+        public abstract List<int> CallContract(NetCall call, NetHash? blockHash = null, int? blockNumber = null);
+
+        public async Task<SentTransactionResponse> SendTransaction(NetInvoke transaction)
         {
             return await this._sendTransaction(transaction);
         }
-
         public abstract DeployAccountTransactionResponse DeployAccount(DeployAccount transaction);
 
         public abstract DeclareTransactionResponse Declare(Declare transaction);
 
-        public abstract int GetClassHashAt(Hash contractAddress, Hash? blockHash = null, int? blockNumber = null);
+        public abstract Task<int> GetClassHashAt(NetHash contractAddress, NetHash? blockHash = null, int? blockNumber = null);
 
-        public abstract Task<int> GetClassHashAt(Hash contractAddress, Hash? blockHash = null, int? blockNumber = null);
+        public abstract Task<CairoContract> GetCairoContractAt(NetHash contractAddress, NetHash? blockHash = null, int? blockNumber = null);
 
-        public abstract Task<ContractClass> GetContractClassAt(Hash contractAddress, Hash? blockHash = null, int? blockNumber = null);
+        public abstract Task<CairoContract> GetCairoContractByHash(NetHash classHash);
 
-        public abstract Task<ContractClass> GetContractClassByHash(Hash classHash);
+        public abstract Task<SierraCairoContract> GetSierraCairoContractAt(NetHash contractAddress, NetHash? blockHash = null, int? blockNumber = null);
 
-        public abstract Task<SierraContractClass> GetSierraContractClassAt(Hash contractAddress, Hash? blockHash = null, int? blockNumber = null);
+        public abstract Task<SierraCairoContract> GetSierraCairoContractByHash(NetHash classHash);
 
-        public abstract Task<SierraContractClass> GetSierraContractClassByHash(Hash classHash);
-
-        protected abstract Task<SentTransactionResponse> _sendTransaction(Invoke transaction);
-
-
+        protected abstract Task<SentTransactionResponse> _sendTransaction(NetInvoke transaction);
     }
 }
